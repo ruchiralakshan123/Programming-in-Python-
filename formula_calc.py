76641ac87d3e9db5a3369dbf5e01f79d1910fc5b
ruchira lakshan <ruchiralakshan2003@gmail.com>
2025-12-09 23:06:04 +0530

    Fix Y calculation and add print statements
    Corrected the calculation of Y and added print statements for y and Z.

--- formula_calc.py
+++ formula_calc.py
@@ -1,14 +1,15 @@
 X = int(input()) 
 
-Y = X*4 + 14
-
+Y = X*4 + 15
+print(y)
 #--------TYPE YOUR CODE TO COMPLETE PROGRAM BELOW THIS LINE----------
-
+#.....y is the ........
 if Y > 30:
     Z = Y * 4
 else:
     Z = Y - 4
 
 #--------TYPE YOUR CODE TO COMPLETE PROGRAM ABOVE THIS LINE----------
-
+print(y)
 print(Z)
+
